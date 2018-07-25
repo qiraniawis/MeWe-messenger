3dfecd87819015102ca9929a4f055f1bdc711e34
arerex14 <arerifxynwa@gmail.com>
2018-07-26 04:11:09 +0800

    setup android project

---
+++ app/src/main/java/com/arerex/mewe_messenger/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.arerex.mewe_messenger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
